19d8aa066ba98f840bc801bca714efe8104550d5
Miguel <pedrosa.miguel@gmail.com>
2022-09-30 18:09:00 +0100

    Adjusted main to max tries using timeout

--- main.cpp
+++ main.cpp
@@ -1,50 +1,56 @@
 #include "Queue.hpp"
 #include <atomic>
 #include <iostream>
+#include <limits>
 #include <thread>
 #include <vector>
+
 
 int main() {
     Queue<int, 8> queue;
     constexpr int totalAmount = 100;
-
-    std::atomic<int> sum = 0, writtenAmount{}, readAmount{};
-    constexpr int expectedSum = totalAmount * (totalAmount + 1) / 2;
+	constexpr int maxTries = 20;
+    std::atomic<long> total = 0;
 
     auto consumer = [&]() {
+        int tries = 0;
+
         while (true) {
-            int i = readAmount;
-            if (i > totalAmount) {
+            if (tries > maxTries) {
                 break;
             }
-            bool canOperate = readAmount.compare_exchange_weak(i, i + 1);
-            if (canOperate) {
-                sum += queue.Pop();
+            if (auto value = queue.Pop()) {
+                total -= *value;
+                tries = 0;
+            } else {
+                tries++;
             }
         }
     };
-
     auto producer = [&]() {
-        while (true) {
-            int i = writtenAmount;
-            if (i > totalAmount) {
+        long tries = 0;
+
+        for (int i = 1; i < totalAmount; /**/) {
+            if (tries > maxTries) {
                 break;
             }
-            bool canOperate = writtenAmount.compare_exchange_weak(i, i + 1);
-            if (canOperate) {
-                queue.Push(i);
+            if (queue.Push(i)) {
+                tries = 0;
+                total += i++;
+            } else {
+                tries++;
             }
         }
     };
 
     std::vector<std::thread> threads;
-    threads.push_back(std::thread(producer));
-    threads.push_back(std::thread(producer));
-    threads.push_back(std::thread(consumer));
+    for (int i = 0; i < 3; i++) {
+        threads.push_back(std::thread(consumer));
+        threads.push_back(std::thread(producer));
+    }
     for (auto& t : threads) {
         t.join();
     }
 
-    std::cout << "Expected outcome was " << expectedSum << '\n';
-    std::cout << "Calculted outcome is " << sum << '\n';
+    std::cout << "Final outcome is " << total << '\n';
 }
