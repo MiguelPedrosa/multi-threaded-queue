84f6d0ba406b3f55f527820518ce758d44c177fe
Miguel <pedrosa.miguel@gmail.com>
2022-09-30 17:03:11 +0100

    Adjusted main to test using threads

--- main.cpp
+++ main.cpp
@@ -1,15 +1,50 @@
 #include "Queue.hpp"
+#include <atomic>
 #include <iostream>
+#include <thread>
+#include <vector>
 
 int main() {
-    Queue<int, 4> queue;
+    Queue<int, 8> queue;
+    constexpr int totalAmount = 100;
 
-    queue.Push(1);
-    queue.Push(2);
-    queue.Push(3);
-    std::cout << "Popped element: " << queue.Pop() << '\n';
-    std::cout << "Popped element: " << queue.Pop() << '\n';
-    queue.Push(4);
-    std::cout << "Popped element: " << queue.Pop() << '\n';
-    std::cout << "Popped element: " << queue.Pop() << '\n';
+    std::atomic<int> sum = 0, writtenAmount{}, readAmount{};
+    constexpr int expectedSum = totalAmount * (totalAmount + 1) / 2;
+
+    auto consumer = [&]() {
+        while (true) {
+            int i = readAmount;
+            if (i > totalAmount) {
+                break;
+            }
+            bool canOperate = readAmount.compare_exchange_weak(i, i + 1);
+            if (canOperate) {
+                sum += queue.Pop();
+            }
+        }
+    };
+
+    auto producer = [&]() {
+        while (true) {
+            int i = writtenAmount;
+            if (i > totalAmount) {
+                break;
+            }
+            bool canOperate = writtenAmount.compare_exchange_weak(i, i + 1);
+            if (canOperate) {
+                queue.Push(i);
+            }
+        }
+    };
+
+    std::vector<std::thread> threads;
+    threads.push_back(std::thread(producer));
+    threads.push_back(std::thread(producer));
+    threads.push_back(std::thread(consumer));
+    for (auto& t : threads) {
+        t.join();
+    }
+
+    std::cout << "Expected outcome was " << expectedSum << '\n';
+    std::cout << "Calculted outcome is " << sum << '\n';
 }
